35b57dc05521b134fb05fedb72c4d1ef8fb79150
murata99 <muratanaganori@realglobe.jp>
2017-12-08 11:36:44 +0900

    edit

--- install_packages.jl
+++ install_packages.jl
@@ -17,11 +17,13 @@ Pkg.add("RCall")
 Pkg.add("ArgParse")
 Pkg.add("SQLite")
 Pkg.add("ProfileView")
-# Pkg.add("IJulia")
-# Pkg.build("ZMQ")
+Pkg.add("JLD")
 
 if try Pkg.installed("Redis") catch nothing end == nothing
-    Pkg.clone("https://github.com/merl-dev/Redis.jl")
+    Pkg.clone("https://github.com/merl-dev/Redis.jl", "Redis")
+    if VERSION.major == 0 && VERSION.minor == 6
+        Pkg.checkout("Redis", "typestable0.6")
+    end
     Pkg.build("Redis")
 end
 
